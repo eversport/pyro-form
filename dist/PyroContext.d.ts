b4a8c1ab1d24b56dea616f4864782ddac38f8bd5
Lukas Kuehnert <lukas.kuehnert@gmail.com>
2018-06-27 12:00:41 +0200

    v0.0.12

--- dist/PyroContext.d.ts
+++ dist/PyroContext.d.ts
@@ -1,12 +1,15 @@
 import React from 'react'
 import { PyroFormErrors, PyroFormTouched } from './typings'
 export declare const getPyroConsumer: <Values extends {}>() => React.StatelessComponent<
-  React.ConsumerProps<Partial<PyroContextProps<Values, keyof Values>>>
+  React.ConsumerProps<Partial<PyroContextProps<Values, Extract<keyof Values, string>>>>
 >
 export declare const PyroProvider: React.ComponentType<
   React.ProviderProps<Partial<PyroContextProps<{}, never>>>
 >
-export interface PyroContextProps<Values = {}, Name extends keyof Values = keyof Values> {
+export interface PyroContextProps<
+  Values = {},
+  Name extends Extract<keyof Values, string> = Extract<keyof Values, string>
+> {
   values: Values
   errors: PyroFormErrors<Values>
   touched: PyroFormTouched<Values>
