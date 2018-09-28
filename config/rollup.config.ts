dfecfbee39e2eed0aac265b7e515e0cd12e6d16f
Lukas Kuehnert <lukas.kuehnert@gmail.com>
2018-09-28 11:57:57 +0200

    Update build config to support es5; Remove dist folder from VCS

--- config/rollup.config.ts
+++ config/rollup.config.ts
@@ -5,27 +5,41 @@ import typescript from 'rollup-plugin-typescript2'
 // @ts-ignore
 import pkg from '../package.json'
 
-const config = {
-  input: './src/index.ts',
-  output: [
-    {
+const config = [
+  {
+    input: './src/index.ts',
+    output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
+
+    external: ['react'],
+
+    plugins: [
+      resolve({
+        jsnext: true,
+        extensions: ['.ts', '.tsx'],
+      }),
+      typescript(),
+    ],
+  },
+
+  {
+    input: './src/index.ts',
+    output: {
+      globals: { react: 'React' },
       name: 'PyroForm',
       file: pkg.browser,
       format: 'umd',
     },
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' },
-  ],
 
-  external: ['react'],
+    external: ['react'],
 
-  plugins: [
-    resolve({
-      jsnext: true,
-      extensions: ['.ts', '.tsx'],
-    }),
-    typescript(),
-  ],
-}
+    plugins: [
+      resolve({
+        jsnext: true,
+        extensions: ['.ts', '.tsx'],
+      }),
+      typescript({ tsconfigOverride: { compilerOptions: { target: 'es5' } } }),
+    ],
+  },
+]
 
 module.exports = config
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default } from './PyroForm'
-export * from './PyroField'
-export * from './PyroContext'
-export * from './helper'
-export * from './typings'
