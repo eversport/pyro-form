336e90013216c7fa9efc08331e167913d8db33fc
Lukas Kuehnert <lukas.kuehnert@gmail.com>
2018-09-28 09:28:10 +0200

    Update dependencies; Small bugfix for automatic value extraction of events

--- src/helper.tsx
+++ src/helper.tsx
@@ -4,4 +4,4 @@ export const getValueFromEvent = (event: React.SyntheticEvent<{ value: string }>
   event.currentTarget.value
 
 export const isEvent = (event: any): event is React.SyntheticEvent<{ value: string }> =>
-  event && event.target && event.target.value && typeof event.target.value === 'string'
+  event && event.target && typeof event.target.value === 'string'
